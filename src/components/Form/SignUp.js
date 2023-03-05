757caa10faf1329b99dd353af535229d1b905146
Saurav Paudel <Sauravpaudel22@gmail.com>
2023-03-05 12:10:32 +0545

    update

--- src/components/Form/SignUp.js
+++ src/components/Form/SignUp.js
@@ -44,28 +44,28 @@ const SignUp = () => {
         <h2>Sign Up</h2>
         <div>
           <div className={errors.name && touched.name ? styles.unCompleted : !errors.name && touched.name ? styles.completed : undefined}>
-            <input type="text" name="name" value={data.name} placeholder="Name" onChange={changeHandler} onFocus={focusHandler} autoComplete="off" />
+            <input type="text" name="name" value={data.name} placeholder="Name" onChange={changeHandler} onFocus={focusHandler} autoComplete="off" required/>
            
           </div>
           {errors.name && touched.name && <span className={styles.error}>{errors.name}</span>}
         </div>
         <div>
           <div className={errors.email && touched.email ? styles.unCompleted : !errors.email && touched.email ? styles.completed : undefined}>
-            <input type="email" name="email" value={data.email} placeholder="E-mail" onChange={changeHandler} onFocus={focusHandler} autoComplete="off" />
+            <input type="email" name="email" value={data.email} placeholder="E-mail" onChange={changeHandler} onFocus={focusHandler} autoComplete="off"required />
            
           </div>
           {errors.email && touched.email && <span className={styles.error}>{errors.email}</span>}
         </div>
         <div>
           <div className={errors.password && touched.password ? styles.unCompleted : !errors.password && touched.password ? styles.completed : undefined}>
-            <input type="password" name="password" value={data.password} placeholder="Password" onChange={changeHandler} onFocus={focusHandler} autoComplete="off" />
+            <input type="password" name="password" value={data.password} placeholder="Password" onChange={changeHandler} onFocus={focusHandler} autoComplete="off" required/>
             
           </div>
           {errors.password && touched.password && <span className={styles.error}>{errors.password}</span>}
         </div>
         <div>
           <div className={errors.confirmPassword && touched.confirmPassword ? styles.unCompleted : !errors.confirmPassword && touched.confirmPassword ? styles.completed : !errors.confirmPassword && touched.confirmPassword ? styles.completed : undefined}>
-            <input type="password" name="confirmPassword" value={data.confirmPassword} placeholder="Confirm Password" onChange={changeHandler} onFocus={focusHandler} autoComplete="off" />
+            <input type="password" name="confirmPassword" value={data.confirmPassword} placeholder="Confirm Password" onChange={changeHandler} onFocus={focusHandler} autoComplete="off" required/>
            
           </div>
           {errors.confirmPassword && touched.confirmPassword && <span className={styles.error}>{errors.confirmPassword}</span>}
